8439121e2147c116ba52184805e69d1c452b1346
Mateo Scarabino <91098568+matescarabino@users.noreply.github.com>
2022-11-09 23:53:28 -0300

    MCGA-01: create base layout and routes

---
+++ src/Components/Products/Form/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from './form.module.css';
+
+const Products = () => {
+  return (
+    <section className={styles.container}>
+        <h2>Form - Se accedera con botones desde Products</h2>
+    </section>
+  )
+};
+
+export default Products;
